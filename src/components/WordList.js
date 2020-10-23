2de14363423f02147a7aa8608ce5a76c9beedc0d
sanford <sanford@Sanfords-MacBook.local>
2020-10-23 16:24:46 -0400

    moved generation of WordList from App.js to WordList.js

--- src/components/WordList.js
+++ src/components/WordList.js
@@ -2,10 +2,24 @@ import React, { Component } from 'react';
 import Word from './Word';
 
 class WordList extends Component {
+
+    state = {
+        wordsToSearch: []
+    }
+
+    componentDidMount() {
+        var appended = this.state.wordsToSearch;
+        var randomWord = require('random-word-by-length');
+        for (var i=0; i<5; i++) {
+            var word = randomWord();
+            appended.push(word);
+        }
+        this.setState({ wordsToSearch: appended });
+    }
+
     render() {
-        var wordsToSearch = this.props.wordsToSearch;
-        console.log(wordsToSearch);
-        return wordsToSearch.map((word) => (
+        var wordList = this.state.wordsToSearch;
+        return wordList.map((word) => (
             <Word wordToSearch={word}/>
         ))
     }
