d43b8809deb4c4987b27583786b7f7854c647b69
sanford <sanford@Sanfords-MacBook.local>
2020-11-12 12:10:01 -0500

    added comments

--- src/components/Word.js
+++ src/components/Word.js
@@ -6,17 +6,26 @@ class Word extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            // state variable to track whether the relevant word data has loaded
             isLoaded: false,
+            // empty json object to store data returned by API call
             wordData: [],
         }
     }
 
+    // called every time the Word component is mounted,
+    // asynchronous because it must wait for the API call to return the json object
     async componentDidMount() {
 
+        // make API call, passing the prop received from WordList component (the word to be searched) as parameter,
+        // and store the json object returned by API call in local variable
         const data = await getWordData(this.props.wordToSearch);
         
+        // set state variables accordingly
         this.setState({
+            // content was loaded, set isLoaded to true
             isLoaded: true,
+            // store json object returned by API call in the wordData variable
             wordData: data,
         })
     }
@@ -25,19 +34,30 @@ class Word extends Component {
         // make local variables to access the state variables
         var { isLoaded, wordData } = this.state;
     
+        // if content is not yet loaded...
         if(!isLoaded) {
+            // return a container that simply indicates that the component is still loading
             return (
                 <div className="container">
                     <p>Loading...</p>
                 </div>
             )
         } 
+        // if the content has loaded...
         else {
+            // organize data from json object into local variables
+            // the json objects for different words are organized uniquely, and may or may not contain the same data
+            // the only consistent data for all words are the part of speech and the definition
+            var word = wordData.entries[0].entry;
+            var partOfSpeech = wordData.entries[0].lexemes[0].partOfSpeech;
+            var definition = wordData.entries[0].lexemes[0].senses[0].definition;
+            
+            // return a container that displays the word data
             return (
                 <div className="container">
-                    <p>{wordData.entries[0].entry} ({wordData.entries[0].lexemes[0].partOfSpeech}) - </p>
+                    <p>{word} ({partOfSpeech}) - </p>
                     
-                    <p>{wordData.entries[0].lexemes[0].senses[0].definition}</p>
+                    <p>{definition}</p>
                 </div>
             );
         }
--- src/components/WordList.js
+++ src/components/WordList.js
@@ -5,38 +5,63 @@ import {getWordData} from './Api.js';
 class WordList extends Component {
 
     state = {
+        // empty array to store string values of words to be searched
         wordsToSearch: []
     }
 
+    // called whenever a WordList component is mounted
+    // generates list of random words to append to wordsToSearch state variable
     componentDidMount() {
 
+        // uses random-word-by-length package from npm to generate a random word with optional max length
         var randomWord = require('random-word-by-length');
+
+        // we want to display 5 words, so iterate 5 times
         for (var i=0; i<5; i++) {
+            // create a variable to hold the string value of a randomly generated word
             var word = randomWord();
+            // make a log of the word we are attempting to add to the list
             console.log("trying word: " + word);
+            // call addToList function, passing the random word we just generated as an argument
             this.addToList(word);
         }
     }
 
+    // an asynchronous, recursive API call to validate that the dictionary we are using is able to return
+    // the necessary data for each word
     async addToList(word) {
         
+        // pass the word as an argument to the getWordData function,
+        // which makes the API call and returns the json object containing data about the word
         const data = await getWordData(word);
 
+        // if the json object is not empty, then the word is usable, and is added to the wordsToSearch state variable
         if (data.entries[0]) {
+            // we cannot manipulate the wordsToSearch array directly, because it is a state variable
+            // so make a dummy array which is equivalent to the current state of the wordsToSearch array,
             var appended = this.state.wordsToSearch;
+            // append new word to dummy array
             appended.push(word);
+            // set wordsToSearch array to the current value of the dummy array (after appending the word)
             this.setState({ wordsToSearch: appended });
         }
+        // if the json object is empty, our API service does not store any info about this word
         else {
+            // instantiate new instance of random-word-by-length package to generate new word
             var randomWord = require('random-word-by-length');
+            // create variable to hold string value of the new word
             var newWord = randomWord();
+            // make a log of the word which failed, and the new word we are attempting to use
             console.log(word + " was not found in dictionary, trying new word: " + newWord);
+            // recursively call the addToList function, this time passing the new word as the argument
             this.addToList(newWord);
         }
     }
 
     render() {
+        // create local variable to access wordsToSearch array from state
         var wordList = this.state.wordsToSearch;
+        // iterate through our list of words, passing each element in the array as a prop to the Word class
         return wordList.map((word) => (
             <Word wordToSearch={word}/>
         ))
